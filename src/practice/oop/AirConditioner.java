8a6fc0f71cd6c5e7f3542718cf46d1ad4acf98f8
jeongyoni <jungyoni990604@gmail.com>
2026-04-27 15:03:12 +0900

    feat: 스마트홈 실습1 기본 기능 구현

--- src/practice/oop/AirConditioner.java
+++ src/practice/oop/AirConditioner.java
@@ -4,48 +4,29 @@ public class AirConditioner {
 
     String name;
     boolean isOn;
-<<<<<<< HEAD
     int temperature;
     String mode;
 
-    void turnOn(){isOn =true;}
-=======
-    int volume;
-
-    void turnOn(){
-        isOn =true;
-    }
->>>>>>> 70a1b1667effdc54d4f58026a6ae9856f86a96e2
-
-    void turnOff(){
-        isOn=false;
+    void turnOn() {
+        isOn = true;
     }
 
-<<<<<<< HEAD
-    void setTemperature(int t){temperature=t;}
+    void turnOff() {
+        isOn = false;
+    }
 
-    void setMode(String m){mode = m;}
+    void setTemperature(int t) {
+        temperature = t;
+    }
 
+    void setMode(String m) {
+        mode = m;
+    }
 
-    void showStatus(){
+    void showStatus() {
         System.out.println(
-                "에어컨("+(isOn? "ON" : "OFF")
-                +","+mode
-                +"," +temperature +"도)");
-=======
-    void volumeUp(){
-        volume += 10;
+                "에어컨(" + (isOn ? "ON" : "OFF")
+                        + "," + mode
+                        + "," + temperature + "도)");
     }
-
-    void volumeDown(){
-        volume -= 10;
-    }
-
-    void showStatus(){
-        System.out.println(
-                "AirConditioner("+(isOn? "ON" : "OFF")
-                +",Vol" +volume + ")");
->>>>>>> 70a1b1667effdc54d4f58026a6ae9856f86a96e2
-    }
-
 }
